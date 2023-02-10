6e900a03d932abc0f617ef81ec27ee5b831a9e57
barretodf <edsb1981@gmail.com>
2023-02-09 23:56:46 -0300

    testes

--- media.js
+++ media.js
@@ -6,21 +6,13 @@
 
 //Calcular a média entre as seguintes notas
 //10 - 6.5 - 8 - 7.5
-/*
+
 const nota1 = 10;
 const nota2 = 6.5;
 const nota3 = 8;
 const nota4 = 7.5;
 
 const media = (nota1 + nota2 + nota3 + nota4) /4;
-*/
-
-const notas = [10, 6.5, 8, 7.5];
-const nota = notas;
-//media das notas
-const media = (nota[0] + nota[1] + nota[2] + nota[3]) /4;
-
-console.log(media)
 
 
 console.log(media)
