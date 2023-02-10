1270132f489bbd72990a2ca2f573f51955303e77
barretodf <edsb1981@gmail.com>
2023-02-10 02:14:18 -0300

    Dividindo alunos em duas salas

--- dividirSala.js
+++ dividirSala.js
@@ -6,6 +6,11 @@ const alunos = ['Helena', 'Alice','Laura','Manuela','Sophia','Isabella',
 'Luísa','Heloísa','Cecília','Mai', 'Eloah', 'Elisa', 'Liz', 'Júlia','Maria',
  'Luísa', 'Valentina','Maria', 'Alice','Manuela','Sophia','Isabella'];
 
+ //vai imprimir da 0 até 12 -1 = fica o total de 12
 const sala1 = alunos.slice(0, 12);
 
-console.log(sala1);
+//vai imprimir do 12 em diante
+const sala2 = alunos.slice(12);
+
+//console.log(sala1);
+console.log(sala2);
