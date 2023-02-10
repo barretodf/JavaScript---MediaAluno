18fbe53d055a0868961b92e0947e4b71857b7ea2
barretodf <edsb1981@gmail.com>
2023-02-10 02:21:39 -0300

    Dividindo cada sala em duas

--- dividirSala.js
+++ dividirSala.js
@@ -1,16 +1,14 @@
-//Usando o método .slice = dividir ou fatiar
-//Só o slice não muda o array original
-//tenho que criar uma variável antes para encapsular ele
+//Dividindo a sala ao meio, caso eu não saiba a quantidade de alunos por ex
 
 const alunos = ['Helena', 'Alice','Laura','Manuela','Sophia','Isabella',
 'Luísa','Heloísa','Cecília','Mai', 'Eloah', 'Elisa', 'Liz', 'Júlia','Maria',
  'Luísa', 'Valentina','Maria', 'Alice','Manuela','Sophia','Isabella'];
 
  //vai imprimir da 0 até 12 -1 = fica o total de 12
-const sala1 = alunos.slice(0, 12);
+const sala1 = alunos.slice(0, alunos.length / 2);
 
 //vai imprimir do 12 em diante
-const sala2 = alunos.slice(12);
+const sala2 = alunos.slice(alunos.length / 2);
 
 console.log(sala1);
 console.log(sala2);
