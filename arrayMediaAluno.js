a5b4ceec3b7cdd411ebb5f1ccfd7e8c17690c43d
barretodf <edsb1981@gmail.com>
2023-02-09 23:21:39 -0300

    Usando a propriedade length

--- arrayMediaAluno.js
+++ arrayMediaAluno.js
@@ -1,9 +1,8 @@
+//Propriedade length
+
 const notas = [10, 6.5, 8, 7.5];
 const nota = notas;
 //media das notas
-const media = (nota[0] + nota[1] + nota[2] + nota[3]) /4;
+const media = (nota[0] + nota[1] + nota[2] + nota[3]) /nota.length;
 
 console.log(media)
-
-//Imprimindo uma das notas
-console.log(nota[3]);
