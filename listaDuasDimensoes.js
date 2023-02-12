ac6d168ad0c71dc3b534f86bcee1dacbb1dff8c6
barretodf <edsb1981@gmail.com>
2023-02-12 00:35:38 -0300

    Criando e acessando listas de duas dimensões

---
+++ listaDuasDimensoes.js
@@ -0,0 +1,8 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const medias = [10, 8, 7.5, 9];
+
+const alunosMedias = [alunos, medias];
+
+console.log(`O aluno da posição 3 da lista de alunos é: ${alunosMedias [0][3]}
+a nota desse aluno é: ${alunosMedias [1][3]}`);
+
