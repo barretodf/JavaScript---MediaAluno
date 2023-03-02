5be210d88973ef8db9575199cc5ebe4bd69464a4
barretodf <edsb1981@gmail.com>
2023-03-01 23:36:20 -0300

    Laço de repetição for com o uso de incremento

---
+++ for.js
@@ -0,0 +1,13 @@
+//Laços de repetição = For
+//Usando incremento
+
+const numeros = [100, 200, 300, 400, 500, 600];
+
+//A primeira expressão é executada uma única vez.
+//A segunda expressão é uma condição de execução.
+//A terceira expressão é executada sempre ao final do bloco
+
+
+for (let indice = 0; indice < numeros.length; indice++) {
+    console.log(numeros[indice]);
+}
