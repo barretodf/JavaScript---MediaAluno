de499662d6930ef205ecf80919b91aaa9740886f
barretodf <edsb1981@gmail.com>
2023-03-02 19:12:04 -0300

    Acrescentando comentário de explicação ao método reduce

--- reduceIndex.js
+++ reduceIndex.js
@@ -2,3 +2,18 @@ const numeros = [43, 50, 65, 12];
 const soma = numeros.reduce((acc, atual) => atual + acc, 0)
 
 console.log(soma)
+
+/*
+O método reduce() está trabalhando com dois parâmetros. 
+O primeiro é a função callback obrigatória para retornar o cálculo e 
+o segundo parâmetro é um número que representa o valor inicial do acumulador – no caso, 0.
+A função callback foi escrita diretamente dentro do reduce(), e 
+esta função também está recebendo dois parâmetros, 
+ambos obrigatórios: o valor acumulado e o valor atual.
+A função callback foi escrita na forma de arrow function; nesse caso, 
+quando só temos uma linha de instrução dentro da função (atual + acc) não precisamos 
+usar chaves e nem da palavra-chave return.
+-------------------------------------------------------------
+Caso você tenha mais de uma linha de instrução dentro de uma arrow function, 
+as chaves {} e a palavra-chave return voltam a ser necessários.
+*/
