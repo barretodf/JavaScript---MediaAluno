04e342891fbec0c204633554ca36b908ac912fc5
barretodf <edsb1981@gmail.com>
2023-03-02 01:10:17 -0300

    Laço de repetição forEach - função callback

---
+++ media-forEach.js
@@ -0,0 +1,14 @@
+//Média com laço de repetição forEach
+//Função callback
+
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0; 
+
+notas.forEach(function (nota) {
+    somaDasNotas += nota;
+});
+
+
+const media = somaDasNotas / notas.length;
+console.log(`A média das notas é: ${media}`);
